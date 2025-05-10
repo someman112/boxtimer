b6480d01f88ae158cd9be088f055eb46d747f9ea
someman112 <abdullahsiddiqui444@gmail.com>
2025-05-09 22:44:13 -0700

    Initial Commit

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by abdsdiddi on 2025-05-09.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
